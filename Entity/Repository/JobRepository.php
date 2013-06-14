72826c7113898798ca63fa9ed65d2eae1b3df815
Ha The Tung <tunght13488@gmail.com>
2013-06-14 16:40:09 +0700

    use UTC datetime

--- Entity/Repository/JobRepository.php
+++ Entity/Repository/JobRepository.php
@@ -175,10 +175,13 @@ class JobRepository extends EntityRepository
             $excludedIds = array(-1);
         }
 
+        $now = new DateTime();
+        $now->setTimezone(new \DateTimeZone('UTC'));
+
         return $this->_em->createQuery("SELECT j FROM JMSJobQueueBundle:Job j LEFT JOIN j.dependencies d WHERE j.executeAfter < :now AND j.state = :state AND j.id NOT IN (:excludedIds) ORDER BY j.id ASC")
                     ->setParameter('state', Job::STATE_PENDING)
                     ->setParameter('excludedIds', $excludedIds)
-                    ->setParameter('now', new DateTime())
+                    ->setParameter('now', $now)
                     ->setMaxResults(1)
                     ->getOneOrNullResult();
     }
